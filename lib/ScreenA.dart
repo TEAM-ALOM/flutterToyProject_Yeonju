724612332092cb6c948b6ac0fe9345b025bfe03d
YJ-Kim03 <160140091+YJ-Kim03@users.noreply.github.com>
2024-03-24 03:06:37 +0900

    fifth

--- lib/ScreenA.dart
+++ lib/ScreenA.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'main.dart';
+import 'package:flutter/services.dart';
+//import 'package:flutter/cupertino.dart';
+//import 'package:hive/hive.dart';
 
-class ScreenA extends StatelessWidget {
+class ScreenA extends StatefulWidget {
+  @override
+  _ScreenAState createState() => _ScreenAState();
+}
+
+class _ScreenAState extends State<ScreenA> {
+  DateTime? _selectedDate;
+  TimeOfDay _startTime = TimeOfDay.now();
+  TimeOfDay _endTime = TimeOfDay.now();
+
+  Future<void> _selectStartTime(BuildContext context) async {
+    final TimeOfDay? newStartTime = await showTimePicker(
+      context: context,
+      initialTime: _startTime,
+    );
+    if (newStartTime != null) {
+      setState(() {
+        _startTime = newStartTime;
+      });
+    }
+  }
+
+  Future<void> _selectEndTime(BuildContext context) async {
+    final TimeOfDay? newEndTime = await showTimePicker(
+      context: context,
+      initialTime: _endTime,
+    );
+    if (newEndTime != null) {
+      setState(() {
+        _endTime = newEndTime;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,46 +65,192 @@ class ScreenA extends StatelessWidget {
               Navigator.pop(context);
             },
           ),
-        ], // actions
+        ],
       ),
-      body: TodoInput(),
-    );
-  }
-}
+      backgroundColor: Colors.deepPurpleAccent.shade100,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "할 일",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w100,
+                ),
+              ),
+              TextFormField(
+                onSaved: (val) {},
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return '할 일을 적어주세요!';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "날짜",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    _selectedDate != null
+                        ? "${_selectedDate!.year}/${_selectedDate!.month}/${_selectedDate!.day}" // 선택한 날짜를 "yyyy/mm/dd" 형식으로 변환
+                        : "",
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black, // 검정색으로 설정
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2030),
+                      ).then(
+                            (selectedDate) {
+                          if (selectedDate != null) {
+                            setState(() {
+                              _selectedDate = selectedDate;
+                            });
+                          }
+                        },
+                      );
+                    },
+                    child: const Text("날짜 선택"),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                "시간",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w200
+                ),
+              ),
+          SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 시작 시간과 종료 시간을 중앙에 정렬하고 사이에 공간을 균등하게 배분
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '시작 시간',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 15,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _selectStartTime(context);
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Text(
+                              "${_startTime.hour}:${_startTime.minute.toString().padLeft(2, '0')}",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20), // 시작 시간과 종료 시간 사이에 공백 추가
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '종료 시간',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 15,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _selectEndTime(context);
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Text(
+                              "${_endTime.hour}:${_endTime.minute.toString().padLeft(2, '0')}",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 50), // 간격 추가
+                  const Text(
+                    "메모",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                  SizedBox(height: 10), // 간격 추가
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10), // 정사각형으로 만들기 위해 BorderRadius.circular로 값을 같게 설정
+                    ),
+                    child: TextFormField(
+                      maxLines: 5, // 다수의 줄을 작성할 수 있는 입력창으로 설정
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: '메모를 입력하세요', // 사용자에게 힌트를 제공
+                      ),
+                    ),
+                  ),
+                ],
+              ),
 
-class TodoInput extends StatefulWidget {
-  @override
-  _TodoInputState createState() => _TodoInputState();
-}
-
-class _TodoInputState extends State<TodoInput> {
-  TextEditingController _textEditingController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          controller: _textEditingController,
-          decoration: InputDecoration(
-            labelText: '할 일',
+            ],
+              ),
           ),
         ),
-        ElevatedButton(
-          onPressed: () {
-            String todo = _textEditingController.text;
-            print('입력된 할 일: $todo');
-
-          },
-          child: Text('추가'),
-        ),
-      ],
     );
-  }
-
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,104 +6,149 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({Key? key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'To Do List',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent.shade100),
         useMaterial3: true,
       ),
-      home:const Home(title:'To Do List'),
+      home: const Home(title: 'To Do List'),
     );
-    }
   }
+}
 
-
-class Home extends StatefulWidget{
-  const Home({super.key, required this.title});
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title});
 
   final String title;
 
   @override
-  State<Home> createState() => HomePage();
+  State<Home> createState() => HomePageState();
 }
 
-
-
-
-class HomePage extends State<Home>{
+class HomePageState extends State<Home> {
+  List<String> currentTasks = [];
+  DateTime startTime = DateTime.now();
+  DateTime endTime = DateTime.now().add(const Duration(hours: 1));
 
   @override
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurpleAccent.shade100,
       appBar: AppBar(
         centerTitle: true,
-        leading: const Icon(Icons.calendar_today_sharp,
-          size: 30,),
-        title: const Text("To Do List",
+        leading: const Icon(
+          Icons.calendar_today_sharp,
+          size: 30,
+        ),
+        title: const Text(
+          "To Do List",
           style: TextStyle(
             fontSize: 23,
             fontWeight: FontWeight.w700,
           ),
         ),
         actions: [
-          IconButton(icon: const Icon(
-            Icons.add,
-            size: 30,
-          ),onPressed:(){
-            Navigator.push(
+          IconButton(
+            icon: const Icon(
+              Icons.add,
+              size: 30,
+            ),
+            onPressed: () {
+              Navigator.push(
                 context,
-                MaterialPageRoute(builder: (_)=>ScreenA())
-            );
-          },
+                MaterialPageRoute(builder: (_) => ScreenA()),
+              );
+            },
           )
-        ], // actions
+        ],
       ),
-      body: const Column(
+      body: Column(
         children: [
-          ExpansionTile(title: Text("오늘의 할 일",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
+          ExpansionTile(
+            title: const Text(
+              "오늘의 할 일",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              ),
             ),
           ),
+          ExpansionTile(
+            title: const Text(
+              "일주일 간 해야 할 일",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
           ),
-          ExpansionTile(title: Text("일주일 간 해야 할 일",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
-            ),),),
-          ExpansionTile(title: Text(
-            "이번 달의 할 일",
-            style: TextStyle(
-              fontSize: 20,
+          ExpansionTile(
+            title: const Text(
+              "이번 달의 할 일",
+              style: TextStyle(
+                fontSize: 20,
+              ),
             ),
-          ),),
-          ExpansionTile(title: Text(
-            "지금 할 일",
-            style: TextStyle(
-              fontSize: 20,
+          ),
+          ExpansionTile(
+            title: const Text(
+              "지금 할 일",
+              style: TextStyle(
+                fontSize: 20,
+              ),
             ),
-          ),),
-          ExpansionTile(title: Text(
-            "오늘의 할 일",
-            style: TextStyle(
-              fontSize: 20,
+            children: currentTasks
+                .map((task) => ListTile(
+              title: Text(task),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  setState(() {
+                    currentTasks.remove(task);
+                  });
+                },
+              ),
+            ))
+                .toList(),
+          ),
+          ExpansionTile(
+            title: const Text(
+              "오늘의 할 일",
+              style: TextStyle(
+                fontSize: 20,
+              ),
             ),
-          ),),
+          ),
         ],
       ),
     );
-
   }
 
+  @override
+  void initState() {
+    super.initState();
+    checkAndAddTask();
+  }
+
+  void checkAndAddTask() {
+    DateTime now = DateTime.now();
+    if (now.isAfter(startTime) && now.isBefore(endTime)) {
+      if (!currentTasks.contains("현재 시간에 할 일")) {
+        setState(() {
+          currentTasks.add("현재 시간에 할 일");
+        });
+      }
+    } else {
+      if (currentTasks.contains("현재 시간에 할 일")) {
+        setState(() {
+          currentTasks.remove("현재 시간에 할 일");
+        });
+      }
+    }
+  }
 }
-
-
-
